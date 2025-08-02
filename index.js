baadc75ff49f3f86e65155c54a13c191fff004d1
Shreyarora964 <sarora179@myseneca.ca>
2025-08-01 20:56:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -174,7 +174,13 @@ async function getBrowser() {
 // Scraping hospitals
 app.get('/api/hospitals', async (req, res) => {
   try {
-    const browser = await getBrowser();
+    //const browser = await getBrowser();
+    const puppeteer = require('puppeteer');
+
+    const browser = await puppeteer.launch({
+          headless: true,
+          args: ['--no-sandbox', '--disable-setuid-sandbox']
+         });
     const page = await browser.newPage();
     await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36");
     await page.goto('https://www.er-watch.ca', { waitUntil: 'networkidle2' });
